e1c56fc340bec742b7829e42a8389a925481ba4a
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-04 14:55:38 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,12 @@
+//------------Date function--------------------
+
+var date = new Date()
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.toLocaleDateString())
+console.log(date.toLocaleString())
+console.log(date.getFullYear())
+console.log(date.getMinutes())
+console.log(date.getUTCFullYear())
+console.log(date.getMonth())
+console.log(date.getDay())
